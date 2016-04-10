6dec7f330b55cda83fd4584685d88cadbc840440
f <f@f-VirtualBox.(none)>
2016-04-10 18:55:35 -0300

    fix comparators BFS and DFS

--- src/itba/edu/ar/algorithm/comparator/DFSComparator.java
+++ src/itba/edu/ar/algorithm/comparator/DFSComparator.java
@@ -6,10 +6,9 @@ import itba.edu.ar.gps.GPSNode;
 
 public class DFSComparator implements Comparator<GPSNode>{
 
-	//Always o1 > o2
 	@Override
 	public int compare(GPSNode o1, GPSNode o2) {
-		return 1;
+		return o2.getDepth()-o1.getDepth();
 	}
 	
 }
